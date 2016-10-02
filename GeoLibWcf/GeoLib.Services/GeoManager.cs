060a35acf78eb6323a074652c739b144c57e0256
riyant <riyant.kurniawan@gmail.com>
2016-10-03 00:00:23 +0800

    create - DataContract, DataMember - ServiceContract, OperationContract - Unit Test with Moq

---
+++ GeoLibWcf/GeoLib.Services/GeoManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeoLib.Contracts;
+using GeoLib.Data;
+
+namespace GeoLib.Services
+{
+    public class GeoManager: IGeoService
+    {
+        private IZipCodeRepository _zipCodeRepository = null;
+        private IStateRepository _stateRepository = null;
+
+        public GeoManager(IZipCodeRepository zipCodeRepository)
+        {
+            _zipCodeRepository = zipCodeRepository;
+        }
+
+        public GeoManager(IStateRepository stateRepository)
+        {
+            _stateRepository = stateRepository;
+        }
+
+        public GeoManager(IZipCodeRepository zipCodeRepository, IStateRepository stateRepository)
+        {
+            _zipCodeRepository = zipCodeRepository;
+            _stateRepository = stateRepository;
+        }
+
+        public ZipCodeData GetZipInfo(string zip)
+        {
+            ZipCodeData zipCodeData = new ZipCodeData();
+
+            IZipCodeRepository zipCodeRepository = _zipCodeRepository ?? new ZipCodeRepository();
+
+            ZipCode zipCodeEntity = zipCodeRepository.GetByZip(zip);
+            if (zipCodeEntity != null)
+            {
+                zipCodeData = new ZipCodeData
+                {
+                    City = zipCodeEntity.City,
+                    State = zipCodeEntity.State.Abbreviation,
+                    ZipCode = zipCodeEntity.Zip
+                };
+            }
+
+            return zipCodeData;
+        }
+
+        public IEnumerable<string> GetStates(bool primaryOnly)
+        {
+            List<string> stateDatas = new List<string>();
+
+            IStateRepository stateRepository = _stateRepository ?? new StateRepository();
+
+            IEnumerable<State> states = stateRepository.Get(primaryOnly);
+            if (states != null)
+            {
+                foreach (var state in states)
+                {
+                    stateDatas.Add(state.Abbreviation);
+                }
+            }
+
+            return stateDatas;
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string state)
+        {
+            List<ZipCodeData> zipCodeDatas = new List<ZipCodeData>();
+
+            IZipCodeRepository zipCodeRepository = _zipCodeRepository ?? new ZipCodeRepository();
+
+            var zips = zipCodeRepository.GetByState(state);
+            if (zips != null)
+            {
+                zipCodeDatas.AddRange(zips.Select(zipCode => new ZipCodeData
+                {
+                    City = zipCode.City, State = zipCode.State.Abbreviation, ZipCode = zipCode.Zip
+                }));
+            }
+
+            return zipCodeDatas;
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string zip, int range)
+        {
+            List<ZipCodeData> zipCodeDatas = new List<ZipCodeData>();
+
+            IZipCodeRepository zipCodeRepository = _zipCodeRepository ?? new ZipCodeRepository();
+
+            ZipCode zipEntity = zipCodeRepository.GetByZip(zip);
+
+            IEnumerable<ZipCode> zips = zipCodeRepository.GetZipsForRange(zipEntity, range);
+            if (zips != null)
+            {
+                zipCodeDatas.AddRange(zips.Select(zipCode => new ZipCodeData
+                {
+                    City = zipCode.City,
+                    State = zipCode.State.Abbreviation,
+                    ZipCode = zipCode.Zip
+                }));
+            }
+
+            return zipCodeDatas;
+        }
+    }
+}
---
+++ GeoLibWcf/GeoLib.Tests/ManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using GeoLib.Contracts;
+using GeoLib.Data;
+using GeoLib.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GeoLib.Tests
+{
+    [TestClass]
+    public class ManagerTest
+    {
+        [TestMethod]
+        public void Test_Zip_Code_Retrival()
+        {
+            Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+            ZipCode zipCode = new ZipCode
+            {
+                City = "LINCOLN PARK",
+                State = new State() { Abbreviation = "NJ" },
+                Zip = "07035"
+            };
+
+            mockZipCodeRepository.Setup(obj => obj.GetByZip("07035")).Returns(zipCode);
+
+            IGeoService geoService = new GeoManager(mockZipCodeRepository.Object);
+
+            ZipCodeData data = geoService.GetZipInfo("07035");
+
+            bool result = (data.City.ToUpper() == zipCode.City)
+                && (data.State == zipCode.State.Abbreviation)
+                && (data.ZipCode == zipCode.Zip);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
